95f3655c9af0e97ff7152714bd6cdec3ef907647
sasha-bolea <sashabol3a@gmail.com>
2026-03-27 00:03:13 +0100

    data ultima uscita

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,13 @@ async function render() {
     if (!release || !release.name) return;
 
     const artista = release.artists.map(a => a.name).join(", ");
+    const date = new Date(release.release_date).toLocaleDateString("it-IT", { day: "2-digit", month: "long", year: "numeric" });
 
     const container = document.querySelector(".ultima-uscita");
     container.innerHTML = `
         <a href="${release.external_urls.spotify}" target="_blank" rel="noopener">
             <div class="release">
-                <div class="release__label">Ultima uscita</div>
+                <div class="release__label"><span>Ultima uscita</span><span>${date}</span></div>
                 <img src="${release.images[0].url}" alt="${release.name}" />
                 <div class="release__titolo">${release.name}</div>
                 <div class="release__artista">${artista}</div>
